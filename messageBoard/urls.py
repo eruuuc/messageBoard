3e1788018e0e6ca0db9236b948179dd7ff9b6b13
erice <eric.ellingson@gmail.com>
2023-09-25 14:48:52 -0400

    update urls.py add posts\urls.py update posts\admin.py add posts\home.html update posts\models.py

--- messageBoard/urls.py
+++ messageBoard/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include # new
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("posts.urls")), # new
 ]
